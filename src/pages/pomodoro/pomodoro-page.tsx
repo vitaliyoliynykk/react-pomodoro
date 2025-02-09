47499cc376e8359cec51f5787716a2089f9b49f2
Vitalii <vitaliyoliynykk@gmail.com>
2025-02-09 17:01:30 +0200

    clock logic setup

--- src/pages/pomodoro/pomodoro-page.tsx
+++ src/pages/pomodoro/pomodoro-page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@chakra-ui/react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { useEffect, useRef, useState } from 'react';
 
 import ClockComponent from '../../components/clock/clock-component';
 
@@ -18,6 +19,7 @@ const Buttons = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
+  margin-top: 24px;
 `;
 
 const buttonStyle = css`
@@ -29,12 +31,61 @@ const buttonStyle = css`
 `;
 
 function PomodoroPage() {
+  const intervalRef = useRef<number | null>(null);
+  const [currentTime, setCurrentTime] = useState<number>(10);
+  const [clockIsRunning, setClockIsRunning] = useState<boolean>(false);
+
+  const intervalDuration = 1000;
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentTime === 0 && intervalRef.current) {
+      clearInterval(intervalRef.current);
+      setClockIsRunning(false);
+    }
+  }, [currentTime]);
+
+  const startClock = () => {
+    intervalRef.current = setInterval(() => {
+      setCurrentTime((prevTime) => prevTime - 1);
+    }, intervalDuration);
+    setClockIsRunning(true);
+  };
+
+  const stopClock = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+      setClockIsRunning(false);
+    }
+  };
+
+  const handleStartStop = (clockIsRunning: boolean) => {
+    if (clockIsRunning) {
+      stopClock();
+      return;
+    }
+
+    startClock();
+  };
+
   return (
     <Container>
-      <ClockComponent currentTime={86} maxTime={120}></ClockComponent>
+      <ClockComponent currentTime={currentTime} maxTime={10}></ClockComponent>
       <Buttons>
-        <Button variant="solid" css={buttonStyle}>
-          Start
+        <Button
+          variant="solid"
+          css={buttonStyle}
+          onClick={() => handleStartStop(clockIsRunning)}
+        >
+          {clockIsRunning ? 'Stop' : 'Start'}
         </Button>
         <Button variant="solid" css={buttonStyle}>
           Skip
