a52bc6ef5d1de5a25c9d026768b09f31979614f6
Vitalii <vitaliyoliynykk@gmail.com>
2025-03-10 13:49:56 +0200

    start/stop

---
+++ src/pages/pomodoro/pomodoro-page.test.tsx
@@ -0,0 +1,72 @@
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { describe, expect, it, vi } from 'vitest';
+
+import PomodoroPage from './pomodoro-page';
+import { pomodoroReducer } from './pomodoro-slice';
+
+const createTestStore = () => {
+  return configureStore({
+    reducer: { pomodoro: pomodoroReducer },
+  });
+};
+
+describe('PomodoroPage', () => {
+  it('WHEN user clicks start button THEN clock should be started', async () => {
+    const store = createTestStore();
+
+    render(
+      <Provider store={store}>
+        <ChakraProvider value={defaultSystem}>
+          <PomodoroPage />
+        </ChakraProvider>
+      </Provider>
+    );
+
+    const dispatchSpy = vi.spyOn(store, 'dispatch');
+
+    await waitFor(() => {
+      const startButton = screen.getByText('Start');
+      void userEvent.click(startButton);
+
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'pomodoro/startClock' })
+      );
+    });
+  });
+
+  it('WHEN user starts the cloclk AND clicks the "Stop" button THEN clock should be stoped', async () => {
+    const store = createTestStore();
+
+    render(
+      <Provider store={store}>
+        <ChakraProvider value={defaultSystem}>
+          <PomodoroPage />
+        </ChakraProvider>
+      </Provider>
+    );
+
+    const dispatchSpy = vi.spyOn(store, 'dispatch');
+
+    await waitFor(() => {
+      const startButton = screen.getByText('Start');
+      void userEvent.click(startButton);
+
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'pomodoro/startClock' })
+      );
+    });
+
+    await waitFor(() => {
+      const startButton = screen.getByText('Stop');
+      void userEvent.click(startButton);
+
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'pomodoro/stopClock' })
+      );
+    });
+  });
+});
