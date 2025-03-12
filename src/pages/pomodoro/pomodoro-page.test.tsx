a6a154269bde1e9a50965e40714527b73fa6cd10
Vitalii <vitaliyoliynykk@gmail.com>
2025-03-12 19:50:40 +0200

    prebuild script

--- src/pages/pomodoro/pomodoro-page.test.tsx
+++ src/pages/pomodoro/pomodoro-page.test.tsx
@@ -14,6 +14,7 @@ const createTestStore = () => {
   });
 };
 
+// TODO: Add mocks for indexDB
 describe('PomodoroPage', () => {
   it('WHEN user clicks start button THEN clock should be started', async () => {
     const store = createTestStore();
